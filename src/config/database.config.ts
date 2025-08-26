447d3f4b784c8dcd178d3b88bfcd6a70d9db1dda
jherson <rodriguezpaul98@gmail.com>
2025-08-26 10:29:56 -0400

    initial configuration implemented

---
+++ src/config/database.config.ts
@@ -0,0 +1,13 @@
+import { TypeOrmModuleOptions } from '@nestjs/typeorm';
+
+export const databaseConfig = (): TypeOrmModuleOptions => ({
+  type: 'postgres',
+  host: process.env.DB_HOST || 'localhost',
+  port: parseInt(process.env.DB_PORT || '5432', 10),
+  username: process.env.DB_USER || 'postgres',
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME || 'TesloDB',  entities: [],  synchronize: true,
+  logging: process.env.NODE_ENV === 'development',
+  dropSchema: false,
+  migrationsRun: false,
+});
