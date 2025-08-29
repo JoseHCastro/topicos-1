7a3ebcd666979b4b1ca21cf2c5c7c6d16b4bdfe0
jherson <rodriguezpaul98@gmail.com>
2025-08-29 16:35:03 -0400

    add the auth decorator to the remaining controllers

--- src/assessments/controllers/grade.controller.ts
+++ src/assessments/controllers/grade.controller.ts
@@ -3,17 +3,21 @@ import { GradeService } from '../services';
 import { CreateGradeDto, UpdateGradeDto } from '../dto';
 import { PaginationDto, PaginatedResultDto } from '../../common';
 import { Grade } from '../entities';
+import { Auth } from '../../auth/decorators';
+import { ValidRoles } from '../../auth/interfaces';
 
 @Controller('grades')
 export class GradeController {
   constructor(private readonly gradeService: GradeService) {}
 
   @Post()
+  @Auth(ValidRoles.ADMIN, ValidRoles.TEACHER)
   create(@Body() createGradeDto: CreateGradeDto) {
     return this.gradeService.create(createGradeDto);
   }
 
   @Get()
+  @Auth(ValidRoles.ADMIN, ValidRoles.TEACHER, ValidRoles.STUDENT)
   findAll(
     @Query() paginationDto: PaginationDto,
     @Query('student_id') studentId?: string,
@@ -29,16 +33,19 @@ export class GradeController {
   }
 
   @Get(':id')
+  @Auth(ValidRoles.ADMIN, ValidRoles.TEACHER, ValidRoles.STUDENT)
   findOne(@Param('id') id: string) {
     return this.gradeService.findOne(id);
   }
 
   @Patch(':id')
+  @Auth(ValidRoles.ADMIN, ValidRoles.TEACHER)
   update(@Param('id') id: string, @Body() updateGradeDto: UpdateGradeDto) {
     return this.gradeService.update(id, updateGradeDto);
   }
 
   @Delete(':id')
+  @Auth(ValidRoles.ADMIN)
   remove(@Param('id') id: string) {
     return this.gradeService.remove(id);
   }
--- src/teaching/controllers/course-section.controller.ts
+++ src/teaching/controllers/course-section.controller.ts
@@ -3,32 +3,39 @@ import { CourseSectionService } from '../services';
 import { CreateCourseSectionDto, UpdateCourseSectionDto } from '../dto';
 import { PaginationDto, PaginatedResultDto } from '../../common';
 import { CourseSection } from '../entities';
+import { Auth } from '../../auth/decorators';
+import { ValidRoles } from '../../auth/interfaces';
 
 @Controller('course-sections')
 export class CourseSectionController {
   constructor(private readonly courseSectionService: CourseSectionService) {}
 
   @Post()
+  @Auth(ValidRoles.ADMIN)
   create(@Body() createCourseSectionDto: CreateCourseSectionDto) {
     return this.courseSectionService.create(createCourseSectionDto);
   }
 
   @Get()
+  @Auth(ValidRoles.ADMIN, ValidRoles.TEACHER, ValidRoles.STUDENT)
   findAll(@Query() paginationDto: PaginationDto): Promise<PaginatedResultDto<CourseSection>> {
     return this.courseSectionService.findAll(paginationDto);
   }
 
   @Get(':id')
+  @Auth(ValidRoles.ADMIN, ValidRoles.TEACHER, ValidRoles.STUDENT)
   findOne(@Param('id') id: string) {
     return this.courseSectionService.findOne(id);
   }
 
   @Patch(':id')
+  @Auth(ValidRoles.ADMIN)
   update(@Param('id') id: string, @Body() updateCourseSectionDto: UpdateCourseSectionDto) {
     return this.courseSectionService.update(id, updateCourseSectionDto);
   }
 
   @Delete(':id')
+  @Auth(ValidRoles.ADMIN)
   remove(@Param('id') id: string) {
     return this.courseSectionService.remove(id);
   }
